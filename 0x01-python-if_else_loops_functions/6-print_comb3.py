acd90b4301af8c53874898949fdb43e2dae1e65a
Hibajk <hibachenni22@gmail.com>
2023-11-28 04:01:57 -0800

    a

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for a in range(0, 10):
+    if a == 8:
+        break
+    for h in range(a, 10):
+        if h == a:
+            continue
+        print('{}{}'.format(a, h), end=", ")
+print('{}{}'.format(a, h))
